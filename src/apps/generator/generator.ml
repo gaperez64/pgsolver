4510e0e42b2ea85a69219c60ce41e670ed231ac0
Guillermo A. Perez <gaperez64@gmail.com>
2025-12-09 08:57:45 +0100

    a bit of cleaning and made the generator argument compulsory

--- src/apps/generator/generator.ml
+++ src/apps/generator/generator.ml
@@ -1,7 +1,6 @@
 open Basics ;;
 open Tcsargs;;
 open Arg ;;
-open Tcstiming;;
 open Paritygame ;;
 open Generators ;;
 
@@ -17,15 +16,14 @@ struct
   let be_very_verbose _ = set_verbosity 3
 
   let genlist = fold_generators (fun _ ident _ t -> t ^ " " ^ ident) ""
-  let genargs = ref ""
+  let genargs = ref []
   let printsolverinfo = ref false
 
   let speclist =  [ (["-v"], Int(set_verbosity),
                       "<level>\n     sets the verbosity level, valid arguments are 0 (quiet), 1 (default), 2 (verbose) and 3 (very verbose)");
                     (["--generator"; "-gen"], 
-                     Tuple [String (fun s -> let (gen, ident) = find_generator s in generator := YesGenerator (ident, gen));
-                            String (fun a -> genargs := a)],
-                     "<generator> \"<args>\"\n     use generator, valid ones are" ^ genlist)]
+                      String (fun s -> let (gen, ident) = find_generator s in generator := YesGenerator (ident, gen)),
+                     "<generator> use generator, valid ones are" ^ genlist) ]
 
   let header = Info.get_title "Parity Game Generator"
 end ;;
@@ -34,22 +32,17 @@ open CommandLine ;;
 
 let _ =
   Random.self_init ();
-  init_message_timing ();
-  SimpleArgs.parsedef speclist (fun _ -> message 1 (fun _ -> "input file")) (header ^ "Usage:
-    generator --generator [family] [parameters]\n" ^
+  SimpleArgs.parsedef speclist (fun a -> genargs := a :: !genargs) (header ^ "Usage:
+    generator --generator <generator family> <args>\n" ^
                                               "Generates parity games to STDOUT.\n");
+  let genargs = List.rev !genargs in
 
-  message 1 (fun _ -> header);
-  let game = match !generator with
-    NoGenerator -> (Parsers.parse_parity_game stdin) |
-    YesGenerator (ident, gen) -> (
-	  message 1 (fun _ -> "Generating " ^ ident ^ " with arguments '" ^ !genargs ^ "'... ");
-	  let timobj = SimpleTiming.init true in
-	  let game = gen (Array.of_list (Tcsstrings.StringUtils.explode !genargs ' ')) in
-	  SimpleTiming.stop timobj;
-	  message 1 (fun _ -> (SimpleTiming.format timobj) ^ "\n");
-	  game
-    )
+  let game =
+  match !generator with
+  | NoGenerator ->
+      failwith "Error: You must specify --generator <generator> <args>."
+  | YesGenerator (_, gen) ->
+      gen (Array.of_list (genargs))
   in
 
   flush stdout;
